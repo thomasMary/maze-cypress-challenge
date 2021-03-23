7b37d5b6e77e4473e7cca428fff6a68b6daf9e17
Thomas <th26.mary@gmail.com>
2021-03-23 13:24:16 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Maze Cypress interview challenge
+
+This report is a starter for the Maze interview challenge.
+
+# Setup
+
+- Clone the repository locally
+- Install the dependencies
+- Run the test to verify thinkgs are working correctly
+
---
+++ cypress/integration/maze/report.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+context('Report', () => {
+    beforeEach(() => {
+      cy.visit('https://6059c258605bdfe57ecadc3a--maze-webapp-app-staging.netlify.app/report/1srgn3qgkhkshsx9');
+    });
+
+    // it('s your turn', () => {
+
+    // });
+});
